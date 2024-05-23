85c6c4796777c80cd08368ca4ea1efbb14e86637
Umer Ahmed Siddiqui <umerahmed4121@gmail.com>
2024-05-23 16:30:39 +0500

    project deployment

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          port: ''
+        }
+      ]
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
